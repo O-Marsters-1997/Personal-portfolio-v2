a90d2c7651e34a093bd36ca5f2def9cac20701be
olly <olly.marsters@yahoo.co.uk>
2023-06-09 21:22:14 +0100

    got my slot working

---
+++ src/hooks/viewport.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export const useMobileLayout = (viewport: number) => {
+  const [isMobileView, setIsMobileView] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const { innerWidth } = window;
+      setIsMobileView(innerWidth < viewport);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return (): void => window.removeEventListener("resize", handleResize);
+  }, [viewport]);
+
+  return isMobileView;
+};
+
+console.log(useMobileLayout(600));
