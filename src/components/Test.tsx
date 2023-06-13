4f190b18a210757c6cfcd93cbf584162177f44d4
olly <olly.marsters@yahoo.co.uk>
2023-06-13 10:37:16 +0100

    fix linting

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,18 +1,18 @@
-import { useState, useEffect, ReactNode } from "react";
+import { useState, useEffect, type ReactNode } from "react";
 
-type Props = {
+interface Props {
   children: ReactNode;
   breakpoint: number;
-};
+}
 
 export default function MySidebar({ breakpoint, children }: Props) {
   const [width, setWidth] = useState(0);
 
   useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
+    const handleResize = () => { setWidth(window.innerWidth); };
     window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
+    return () => { window.removeEventListener("resize", handleResize); };
   }, []);
 
   return <aside>{width > breakpoint && <main>{children}</main>}</aside>;
