e5b33220f3ca3b2a1e08a135749121c87a02051f
olly <olly.marsters@yahoo.co.uk>
2023-06-18 16:40:24 +0100

    refactor script

--- src/scripts/generateTailwindTheme.cjs
+++ src/scripts/generateTailwindTheme.cjs
@@ -6,17 +6,18 @@ const path = require("path");
 const tailwindConfig = require("../../tailwind.config.cjs");
 
 const { theme } = resolveConfig(tailwindConfig);
-const themeStr = JSON.stringify(theme);
-const js = `
-const theme  = ${themeStr}
+const themeStr = JSON.stringify(theme.colors);
+const ts = `
+// Make sure the TailwindColors type is defined
+const theme : TailwindColors  = ${themeStr}
 export default theme
 `;
 
 try {
   // write the file using fs
   fs.writeFileSync(
-    path.resolve(process.cwd(), "./src/styles/theme.js"),
-    prettier.format(js, { parser: "babel" }),
+    path.resolve(process.cwd(), "./src/styles/theme.ts"),
+    prettier.format(ts, { parser: "babel" }),
     "utf-8"
   );
 } catch (error) {
--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -1,1 +1,5 @@
 type Viewport = "mobile" | "small-tablet" | "tablet" | "desktop";
+
+type TailwindColors =
+  | ResolvableTo<RecursiveKeyValuePair<string, string>>
+  | undefined;
