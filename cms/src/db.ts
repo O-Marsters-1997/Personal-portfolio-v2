5dd1927553765f63e384f2c6af91354ceb5ef834
olly-personal <olly.marsters@yahoo.co.uk>
2024-10-22 20:01:27 +0100

    removing unauthored workflows

--- cms/src/db.ts
+++ cms/src/db.ts
@@ -5,7 +5,7 @@ export const getConnectionString = () => {
     NODE_ENV === "production" ? DB_HOST : `${DB_HOST}:${DB_PORT}`;
 
   const connString = `postgresql://${DB_USER}:${DB_PASSWORD}@${hostName}/${process.env.DB_NAME}`;
-
+  //
   if (NODE_ENV === "production") {
     return `${connString}?sslmode=require`;
   }
