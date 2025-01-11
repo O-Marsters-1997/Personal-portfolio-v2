0459e8ec5ea11df8b9fe7e00845e9c21ec841047
olly-personal <olly.marsters@yahoo.co.uk>
2025-01-11 11:36:22 +0000

    reattempting ci

--- personal_site/astro.config.mjs
+++ personal_site/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-    adapter: vercel({
-      imageService: true,
+  adapter: vercel({
+    imageService: true,
   }),
 });
