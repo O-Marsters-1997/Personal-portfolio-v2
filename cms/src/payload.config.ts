27c5aa56409d32ee8a643fb2362a5f585bdb9f31
olly-personal <olly.marsters@yahoo.co.uk>
2025-04-12 20:43:42 +0100

    testing ci

--- cms/src/payload.config.ts
+++ cms/src/payload.config.ts
@@ -33,8 +33,5 @@ export default buildConfig({
     },
   }),
   sharp,
-  plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
-  ],
+  plugins: [payloadCloudPlugin()],
 })
--- cms/src/utils/index.ts
+++ cms/src/utils/index.ts
@@ -3,5 +3,3 @@ export const getConnectionString = () => {
 
   return `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}${NODE_ENV === 'production' ? 'ssl' : ''}`
 }
-
-// testing this
